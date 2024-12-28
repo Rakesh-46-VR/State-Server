67ad0b151e4c837bcf1af03d0c30d99f8a46f387
Rakesh-46-VR <rakeshbsirvi46@gmail.com>
2024-12-28 13:43:47 +0530

    added password

--- index.js
+++ index.js
@@ -1,8 +1,12 @@
 const express = require("express");
+const dotenv = require("dotenv");
+
+dotenv.config();
 
 const app = express();
 const bodyParser = require("body-parser");
 const PORT = 3000;
+
 let state = 0;
 
 // EXPRESS SPECIFIC STUFF
@@ -16,19 +20,28 @@ app.use(
 
 app.use(bodyParser.json());
 
-app.get("/", (req, res) => {
-  res.status(200).json({ state });
+app.get("/:password", (req, res) => {
+  const { password } = req.params;
+  if (password === process.env.PASSWORD) {
+    res.status(200).json({ state });
+  } else {
+    res.status(400).json({ info: "Missing password" });
+  }
 });
 
-app.get("/set/state=:stateValue", (req, res) => {
-  const { stateValue } = req.params;
-  // Attempt to parse it as an integer (optional, if you need an integer)
-  const numericState = parseInt(stateValue, 10);
-  if (!isNaN(numericState)) {
-    state = numericState;
-    res.status(200).json({ status: true });
+app.get("/set/:stateValue/:password", (req, res) => {
+  const { stateValue, password } = req.params;
+  if (password === process.env.PASSWORD) {
+    // Attempt to parse it as an integer (optional, if you need an integer)
+    const numericState = parseInt(stateValue, 10);
+    if (!isNaN(numericState)) {
+      state = numericState;
+      res.status(200).json({ status: true });
+    } else {
+      res.status(400).json({ status: false });
+    }
   } else {
-    res.status(400).json({ status: false });
+    res.status(400).json({ info: "Missing password" });
   }
 });
 
